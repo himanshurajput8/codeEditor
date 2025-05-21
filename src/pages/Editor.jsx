0d85c56259674299f3980fd1a0df9f1a1448fcab
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-05-21 14:42:40 +0530

    fixs

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -13,7 +13,7 @@ export const EditorComp = () => {
     useEffect(() => {
         // Connect to backend
         // socketRef.current = io('http://localhost:5001'); 
-        socketRef.current = io('https://your-backend-domain.onrender.com:10000', {
+        socketRef.current = io('https://your-backend-domain.onrender.com', {
             withCredentials: true,
             transports: ['websocket'],
         });
