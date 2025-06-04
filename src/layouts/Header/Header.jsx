87faa3080b951f3a1d6e1019233b9682b3dbde20
Wannacryy24 <mayankverma.dev@gmail.com>
2025-06-04 11:30:27 +0530

    added Cursor Functionaity

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,59 +1,72 @@
-
-import React, { useContext, useState } from 'react'
-import './header.css'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useState, useEffect, useRef } from 'react';
+import './header.css';
+import { useNavigate } from 'react-router-dom';
 import { userNameContext } from '../../ContextAPI/UserNameContext';
 import UserNameModal from '../ModalComponent/UserNameModal';
 
 export const Header = () => {
     const [showDropDown, setShowDropDown] = useState(false);
-    const { showModal, setShowModal } = useContext(userNameContext);
-
+    const { showModal, setShowModal, userName } = useContext(userNameContext);
     const navigate = useNavigate();
-    const handleGoToHome = () => {
-        navigate('/')
-    }
 
-    const { userName } = useContext(userNameContext);
+    const dropdownRef = useRef(null);
 
-    const handleBtnClick = () => {
-        setShowDropDown(!showDropDown);
-    }
+    const handleGoToHome = () => navigate('/');
 
-    const handleForModal = () => {
+    const toggleDropDown = () => setShowDropDown(prev => !prev);
+
+    const openUserNameModal = () => {
         setShowDropDown(false);
         setShowModal(true);
     };
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setShowDropDown(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
         <header className="header">
-            <div className="logo"
-                onClick={handleGoToHome}
-            >
-                <img src="/curly.png" alt="" />
+            <div className="logo" onClick={handleGoToHome} role="button" tabIndex={0}>
+                <img src="/curly.png" alt="logo" />
                 Code2gthr
             </div>
-            {
-                userName ? (
-                    <div className='header-userName-dropDown'>
-                        <p>{userName}</p>
-                        <button
-                            onClick={() => handleBtnClick()}
-                        ><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" /></svg></button>
-                     {showDropDown && (
-            <div className="dropdown">
-              <p onClick={handleForModal}>Change User Name</p>
-              <p>Login</p>
-              <p>Sign Up</p>
-            </div>
-          )}
-        </div>
-      ) : (
-        <p>Guest SignIn</p>
-      )}
 
-      {showModal && <UserNameModal />}
-    </header>
-  )
-}
+            {userName ? (
+                <div className="header-userName-dropDown" ref={dropdownRef}>
+                    <p>{userName}</p>
+                    <button
+                        onClick={toggleDropDown}
+                        aria-haspopup="true"
+                        aria-expanded={showDropDown}
+                        aria-label="User menu"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000">
+                            <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+                        </svg>
+                    </button>
+                    {showDropDown && (
+                        <div className="dropdown">
+                            <p onClick={openUserNameModal}>Change User Name</p>
+                            <p >Login</p>
+                            <p >Sign Up</p>
+                        </div>
+                    )}
+                </div>
+            ) : (
+                <button className="guest-signin" onClick={() => navigate('/signin')}>
+                    Sign In as Guest
+                </button>
+            )}
+
+            {showModal && <UserNameModal />}
+        </header>
+    );
+};
